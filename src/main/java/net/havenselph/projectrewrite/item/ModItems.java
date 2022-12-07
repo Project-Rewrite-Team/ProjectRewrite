b60783e8f72de149c975bff343f35021a7908f70
HavenSelph <havenselph@gmail.com>
2022-12-07 02:37:58 -0600

    Fixed duplicate item identifier

--- src/main/java/net/havenselph/projectrewrite/item/ModItems.java
+++ src/main/java/net/havenselph/projectrewrite/item/ModItems.java
@@ -13,7 +13,7 @@ public class ModItems {
     // Yucca Wood Family
     public static final Item YUCCA_PALM_BOAT = registerItem("yucca_palm_boat", new Item(new FabricItemSettings().group(ModItemGroup.PROJECT_REWRITE)));
     public static final Item YUCCA_PALM_CHEST_BOAT = registerItem("yucca_palm_chest_boat", new Item(new FabricItemSettings().group(ModItemGroup.PROJECT_REWRITE)));
-    public static final Item YUCCA_PALM_SIGN = registerItem("yucca_palm_chest_boat", new SignItem(new FabricItemSettings().group(ModItemGroup.PROJECT_REWRITE).maxCount(16), ModBlocks.YUCCA_PALM_SIGN, ModBlocks.YUCCA_PALM_WALL_SIGN));
+    public static final Item YUCCA_PALM_SIGN = registerItem("yucca_palm_sign", new SignItem(new FabricItemSettings().group(ModItemGroup.PROJECT_REWRITE).maxCount(16), ModBlocks.YUCCA_PALM_SIGN, ModBlocks.YUCCA_PALM_WALL_SIGN));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(ProjectRewrite.MOD_ID, name), item);
